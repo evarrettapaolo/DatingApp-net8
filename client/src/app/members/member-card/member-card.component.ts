eaebae3a68b291f105f568f93779dd3b81d4f7d4
Paolo Evarretta <evarretta11paolo@gmail.com>
2024-10-18 00:51:16 -0400

    End of Section 11

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -7,45 +7,7 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './member-card.component.html',
-  styles: [
-    `
-      .card:hover img {
-        transform: scale(1.2, 1.2);
-        transition-duration: 500ms;
-        transition-timing-function: ease-out;
-      }
-
-      .card img {
-        transform: scale(1, 1);
-        transition-duration: 500ms;
-        transition-timing-function: ease-out;
-      }
-
-      .card-img-wrapper {
-        overflow: hidden;
-        position: relative;
-      }
-
-      .member-icons {
-        position: absolute;
-        bottom: -30%;
-        left: 0;
-        right: 0;
-        margin-right: auto;
-        margin-left: auto;
-        opacity: 0;
-      }
-
-      .card-img-wrapper:hover .member-icons {
-        bottom: 0;
-        opacity: 1;
-      }
-
-      .animate {
-        transition: all 0.3s ease-in-out;
-      }
-    `,
-  ],
+  styleUrl: './member-card.component.scss',
 })
 export class MemberCardComponent {
   member = input.required<Member>();
